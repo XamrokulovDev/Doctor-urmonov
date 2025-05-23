13bcf640e0731b30614a491c5894514b1aaadcd9
ulfatjon <xamrokulovulfatjon2005@gmail.com>
2025-05-23 15:38:07 +0500

    The project is finished.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,5 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
@@ -11,6 +10,10 @@ i18n
   .init({
     fallbackLng: "uz",
     debug: true,
+
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json"
+    },
 
     interpolation: {
       escapeValue: false,
